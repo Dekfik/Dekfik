8132806352fee42fdc29fb7b77566b91bc917413
Dekfik <96250845+Dekfik@users.noreply.github.com>
2021-12-16 19:23:06 +0400

    HomeWorkOne
    HomeWorkOne

---
+++ MyHomeWorkOne.java
@@ -0,0 +1,47 @@
+class MyHomeWorkOne {
+	static avoid main (String[] args) {
+	    System.out.println ("MyHomeWorkOne");
+	}
+	    Orange();
+	static.void.println("Orange")
+	    Banana();
+	static.void.println("Banana")
+	    Apple();
+	static.void.println("Apple")
+	
+static void main (String[] args) {
+	int a = -25
+	int b = 50
+	if (a + b>=0) {
+		System.out.println("Sum is positive")
+	} else {
+		System.out.println("Sum in negative")
+	}
+class PrintColor {
+static avoid main (String[] args) {
+	System.out.println ("PrintColor");
+}
+	static void main (String[] args) {
+	int T = 55
+	if (T <= 0) {
+	    System.out.println("Red")}
+	if (T >0; <=100) {
+		System.out.println ("Yellow")
+	if (T>100) {
+		System.out.println ("Green")	
+}
+class CompareNumbers {
+    static avoid main (String[] args) {
+    System.out.println ("CompareNumbers")
+}
+	static void main (String[] args) {
+	int A = 10
+	int B = 30
+	if (A >= B) {
+	    System.out.println ("a >= b") 
+	} else {
+		System.out.println ("a < b" 
+	}
+		
+		
+		}
